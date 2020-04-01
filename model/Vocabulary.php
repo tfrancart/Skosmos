2098a97673af903e27bd5ec04dd49b9afb129ea8
Thomas Francart <thomas.francart@sparna.fr>
2020-04-01 14:59:50 +0200

    Unsesco brackets rtl (#18)
    * Style et Date.
    
    Logo  et colonnes inversés rtl.
    Occurences de recherche.
    Date : setDefaultLocale déplacé de Concept.php vers WebController.php.
    
    * Alphabet.
    
    Inversion de l'ordre du tableau contenant les lettres.
    
    * Arrow direction arabic.
    
    Arrow character switch from right arrow to left arrow in case of ar lang.
    
    * Add 'table' class on #counts and set same max-width as #statistics table
    
    * Update table counts
    
    * Fix brackets display rtl
    
    Co-authored-by: GabExperiencis <gabriel@experiencis.fr>

--- model/Vocabulary.php
+++ model/Vocabulary.php
@@ -480,17 +480,6 @@ class Vocabulary extends DataObject
         $lang = $lang ? $lang : $this->getEnvLang();
 
 
-        foreach ($chars as $char) {
-            if (preg_match('/\p{L}/u', $char)) {
-                $letters[] = $char;
-            } elseif (preg_match('/\d/u', $char)) {
-                $digits = true;
-            } else {
-                $specials = true;
-            }
-        }
-
-
         // ***** Reverse Array for arabic alphabet ***** 
         if(  $clang == "ar" && $lang == "ar") {
             usort($letters, 'strcoll');
